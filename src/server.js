a5b37512c917b4a78d1ef85c0807defe10ca36e6
onejune lee <lee1june61@kaist.ac.kr>
2023-09-16 06:15:03 +0000

    initialization

---
+++ src/server.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema.js";
+import { getUser } from "./users/users.utils.js";
+import http from "http";
+
+const PORT = process.env.PORT
+
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async (ctx) => {
+    if (ctx.req) {
+      return {
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
+      }
+    }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("please login to listen.");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
+  },
+  formatError: (err) => {
+    console.log(err);
+  },
+});
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/healthcheck', require('express-healthcheck')());
+
+app.use(logger("tiny"));
+apollo.applyMiddleware({ app });
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: PORT }, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
