fdd3bbf4fcaf41302eeef569427e81f672a98ba4
onejune lee <lee1june61@kaist.ac.kr>
2023-09-22 13:06:36 +0000

    createAccount typeDefs

---
+++ src/users/createAccount/createAccount.typeDefs.js
@@ -0,0 +1,16 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type createAccountResult {
+    ok: Boolean!
+    error: String
+    token: String
+    userId: Int
+  }
+  type Mutation {
+    createAccount(
+      username: String!
+      password: String!
+    ): createAccountResult!
+  }
+`;
