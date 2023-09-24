ecc9ffaf771376093a0514fb140f407a7e29e3a1
Gwemin <dbwnghks921@naver.com>
2023-09-24 04:26:05 +0000

    [db] chat, post schema add

---
+++ db/posts/post.schema.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+
+const PostSchema = Schema(
+  {
+    title: { type: String, require: true, trim: true },
+    content: { type: String, require: true, maxlength: 500, trim: true },
+    writer: { type: String, require: true },
+    recommand: { type: Number, require: true, default: 0 },
+    share: { type: Number, require: true, default: 0 },
+    Comment: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "comment",
+      },
+    ],
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date },
+  },
+  { collection: "post", timestamps: true }
+);
+
+export const PostModel = model("Post", PostSchema);
+
+const CommentSchema = Schema(
+  {
+    text: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date },
+    // 댓글의 글은 1개이므로 배열x
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "post",
+    },
+  },
+  { collection: "comment" }
+);
+
+export const CommentModel = model("Comment", CommentSchema);
