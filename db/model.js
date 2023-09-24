ea769435e247d0a5dec9981382b29448eb177d1f
Gwemin <dbwnghks921@naver.com>
2023-09-24 14:54:25 +0000

    [db] roomModel 추가

--- db/model.js
+++ db/model.js
@@ -1,18 +1,20 @@
 import { model } from "mongoose";
 import UserSchema from "./user/user.schema";
 import { CommentSchema, PostSchema } from "./posts/post.schema";
-import { ChatSchema } from "./chats/chat.schema";
+import { ChatSchema, RoomSchema } from "./chats/chat.schema";
 
 const userModel = model("UserSchema", UserSchema);
 const postModel = model("PostSchema", PostSchema);
 const commentModel = model("CommentSchema", CommentSchema);
 const chatModel = model("ChatSchema", ChatSchema);
+const roomModel = model("RoomModel", RoomSchema);
 
 const dbModel = {
   user: userModel,
   post: postModel,
   comment: commentModel,
   chat: chatModel,
+  room: roomModel,
 };
 
 export default dbModel;
