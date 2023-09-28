130f6588d24a7465276ec38deb9a737e567f5381
Gwemin <dbwnghks921@naver.com>
2023-09-28 01:59:40 +0000

    [post] 게시글 생성 인자[title, text, writerNickname]

--- src/posts/createPost/createPost.typeDefs.js
+++ src/posts/createPost/createPost.typeDefs.js
@@ -1,12 +1,17 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type createPostResult {
+    ok: String
+    error: String
+    message: String
+  }
+
   type Mutation {
     createPost(
       title: String!
       text: String!
       writerNickname: String!
-      writer_id: String!
-    ): Post
+    ): createPostResult
   }
 `;
