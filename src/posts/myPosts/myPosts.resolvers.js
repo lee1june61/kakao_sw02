551369fdd76ff435250bdefcd8a83eb324629ae6
Gwemin <dbwnghks921@naver.com>
2023-09-28 02:02:40 +0000

    [post] 내가 작성한 게시물 찾는 Query

---
+++ src/posts/myPosts/myPosts.resolvers.js
@@ -0,0 +1,29 @@
+import dbModel from "../../../db/model";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    myPosts: protectedResolver(
+      async (_, { writerNickname }, { loggedInUser }) => {
+        try {
+          if (!writerNickname === loggedInUser.nickname) {
+            throw "사용자 게시물이 아닙니다.";
+          }
+          const posts = await dbModel.post.find({ writerNickname });
+          console.dir(posts);
+          return {
+            ok: true,
+            message: "해당 사용자의 게시물을 가져왔습니다.",
+            posts,
+          };
+        } catch (error) {
+          console.error(e);
+          return {
+            ok: false,
+            message: "해당하는 게시물은 존재하지 않습니다.",
+          };
+        }
+      }
+    ),
+  },
+};
