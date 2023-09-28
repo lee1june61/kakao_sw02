9e13f11b6efea10255cec7f1a8059d595c58b9b6
Gwemin <dbwnghks921@naver.com>
2023-09-28 02:00:54 +0000

    [post] 게시물 삭제 인자[postId, writernicname]

--- src/posts/deletePost/deletePost.resolvers.js
+++ src/posts/deletePost/deletePost.resolvers.js
@@ -1,21 +1,27 @@
 import dbModel from "../../../db/model";
+import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Mutation: {
-    deletePost: async (_, { postNumber, writerNickname }, info) => {
-      try {
-        const post = await dbModel.post.findOneAndDelete(postNumber, writerNickname);
-        return {
-          ok: true,
-          message: `"${post.title}" 게시물을 삭제하였습니다.`,
-        };
-      } catch (e) {
-        console.error(e);
-        return {
-          ok: false,
-          message: `해당 게시물을 삭제하지 못하였습니다.`,
-        };
+    deletePost: protectedResolver(
+      async (_, { postId, writerNickname }, { loggedInUser }) => {
+        try {
+          if (!(loggedInUser.nickname === writerNickname)) {
+            throw "작성한 유저가 아닙니다.";
+          }
+          const post = await dbModel.post.findOneAndDelete({ postId });
+          return {
+            ok: true,
+            message: `"${post.title}" 게시물을 삭제하였습니다.`,
+          };
+        } catch (e) {
+          console.error(e);
+          return {
+            ok: false,
+            message: `해당 게시물을 삭제하지 못하였습니다.`,
+          };
+        }
       }
-    },
+    ),
   },
 };
