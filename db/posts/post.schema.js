94da9631d80e643d0404ebd28e33f719b6ac5038
Gwemin <dbwnghks921@naver.com>
2023-09-28 14:21:45 +0000

    [post, comment[ schema document name변경

--- db/posts/post.schema.js
+++ db/posts/post.schema.js
@@ -2,33 +2,24 @@ import { Schema, model } from "mongoose";
 export const PostSchema = Schema(
   {
     title: { type: String, require: true, trim: true },
-    postId: { type: Number, require: true }, // postId 는 schema가 생성될 경우 +1 씩 증가되어야 한다.
+    postId: { type: Number, require: true, unique: true }, // postId 는 schema가 생성될 경우 +1 씩 증가되어야 한다.
     text: { type: String, require: true, maxlength: 500 },
     writerNickname: { type: String, require: true },
-    writer_id: { type: Schema.Types.ObjectId, ref: "user", require: true },
+    writerId: { type: Schema.Types.ObjectId, ref: "user", require: true },
     recommend: { type: Number, require: true, default: 0 },
     share: { type: Number, require: true, default: 0 },
-    Comment: [{ type: Schema.Types.ObjectId, ref: "comment" }],
-    createdAt: { type: Date, default: Date.now() },
-    updatedAt: { type: Date },
+    commentCnt: { type: Number, require: true, default: 0 },
   },
   { collection: "post", timestamps: true }
 );
 
 export const CommentSchema = Schema(
   {
-    text: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    createdAt: { type: Date, default: Date.now() },
-    updatedAt: { type: Date },
+    text: { type: String, require: true },
+    writerNickname: { type: String, require: true },
+    writerId: { type: Schema.Types.ObjectId, ref: "user", require: true },
     // 댓글의 글은 1개이므로 배열x
-    post: {
-      type: Schema.Types.ObjectId,
-      ref: "post",
-    },
+    postId: { type: Number, ref: "post", require: true },
   },
-  { collection: "comment" }
+  { collection: "comment", timestamps: true }
 );
